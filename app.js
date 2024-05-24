4e5cc1304f4c42e0de9067a1fa18bd3869440ebf
Clifford Musyoka <your_email@example.com>
2024-05-24 16:19:48 +0300

    Add item to the list

--- app.js
+++ app.js
@@ -18,28 +18,48 @@ form.addEventListener("submit", addItem);
 function addItem(e) {
   e.preventDefault();
   const value = grocery.value;
-  
+
   const id = new Date().getTime().toString();
   if (value && !editFlag) {
-    console.log("Add item to list");
+    const element = document.createElement("article");
+    //add class
+    element.classList.add("grocery-item");
+    // add id
+    const attr = document.createAttribute("data-id");
+    attr.value = id;
+    element.setAttributeNode(attr);
+    element.innerHTML = `<p class="title">${value}</p>
+    <div class="btn-container">
+      <button type="button" class="edit-btn">
+        <i class="fas fa-edit"></i>
+      </button>
+      <button type="button" class="delete-btn">
+        <i class="fas fa-trash"></i>
+      </button>
+    </div>`;
+    //append child
+    list.appendChild(element);
+    // display alert
+    displayAlert("item added to the list", "success");
+    //show container
+    container.classList.add("show-container");
   } else if (value && editFlag) {
     console.log("eddirtting");
   } else {
-   displayAlert("Please enter value", "danger");
-   
+    displayAlert("Please enter value", "danger");
   }
 }
 
 //display Alert
-function displayAlert(text, action){
+function displayAlert(text, action) {
   alert.textContent = text;
   alert.classList.add(`alert-${action}`);
 
   //remove alert
-setTimeout(function () {
-  alert.textContent = '';
-  alert.classList.remove(`alert-${action}`);
-}, 1000);
+  setTimeout(function () {
+    alert.textContent = "";
+    alert.classList.remove(`alert-${action}`);
+  }, 1000);
 }
 // ****** LOCAL STORAGE **********
 
